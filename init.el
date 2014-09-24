7071490a25cc75c171fe10213c6421c64d2bac93
Michael Courage <courage@chromium.org>
2014-09-23 17:09:48 -0700

    most of the useful basics, except undo-tree and ftf

--- init.el
+++ init.el
@@ -1,13 +1,9 @@
 ;;----------------------------------------------------------------------
+;; detect system type
+(defvar terminal-p (not (display-graphic-p)))
+
+;;----------------------------------------------------------------------
 ;; Frame and window management
-
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-
-(show-paren-mode 1)
-(setq visible-bell t)
-
-(setq special-display-regexps nil)
 
 (defun select-next-window ()
   "Switch to the next window"
@@ -67,9 +63,59 @@ will be in the middle of the new layout."
   (follow-mode 1))
 
 ;;----------------------------------------------------------------------
+;; Configuration and Minor Modes
+
+;; Remove annoying space consumers
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+;; Stop beeping at me
+(setq visible-bell t)
+
+;; Help me count parens
+(show-paren-mode 1)
+
+;; Who should display in a special frame? No one.
+(setq special-display-regexps nil)
+
+;; create the autosave dir if necessary, since emacs won't.
+(make-directory "~/.emacs.d/autosaves/" t)
+
+;; always revert files that change outside the editor
+(global-auto-revert-mode t)
+
+;; display column numbers
+(column-number-mode)
+
+;; make the mouse work in the terminal
+(when terminal-p
+  (require 'mouse)
+  (xterm-mouse-mode t)
+  (defun track-mouse (e)))
+
+;; Easier buffer disambiguation
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'reverse)
+
+;; interactively do things (and icomplete)
+(require 'ido)
+(ido-mode t)
+(setq ido-enable-flex-matching t)
+(setq ido-use-filename-at-point 'guess)
+(icomplete-mode 1)
+
+;;----------------------------------------------------------------------
 ;; key bindings
 
 (global-set-key (kbd "C-c 3") 'split-into-two-columns-and-cycle)
 (global-set-key (kbd "C-c C-3") 'split-into-two-columns-and-follow)
 (global-set-key (kbd "C-c 4") 'split-into-three-columns-and-cycle)
 (global-set-key (kbd "C-c C-4") 'split-into-three-columns-and-follow)
+
+;;----------------------------------------------------------------------
+;; Custom variables (don't make more than one of these).
+
+(custom-set-variables
+  '(auto-save-file-name-transforms '((".*" "~/.emacs.d/autosaves/\\1" t)))
+  '(backup-directory-alist '((".*" . "~/.emacs.d/backups/")))
+  '(cr-flymake-ninja-build-file "out/Debug/build.ninja"))

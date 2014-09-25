e41cfd440110b4510c25448119a27162062097b1
Michael Courage <courage@chromium.org>
2014-09-25 01:01:42 -0700

    move window splitting functions into a separate file

--- init.el
+++ init.el
@@ -3,66 +3,6 @@
 (defvar terminal-p (not (display-graphic-p)))
 
 ;;----------------------------------------------------------------------
-;; Frame and window management
-
-(defun select-next-window ()
-  "Switch to the next window"
-  (interactive)
-  (select-window (next-window)))
-
-(defun select-previous-window ()
-  "Switch to the previous window"
-  (interactive)
-  (select-window (previous-window)))
-
-(defun split-into-two-columns ()
-  "Split the frame into two columns"
-  (interactive)
-  (delete-other-windows)
-  (split-window-horizontally))
-
-(defun split-into-two-columns-and-cycle ()
-  "Split the frame into two columns and select the next buffer in the second window."
-  (interactive)
-  (split-into-two-columns)
-  (select-next-window)
-  (next-buffer)
-  (select-next-window))
-
-(defun split-into-two-columns-and-follow ()
-  "Split the frame into two columns and enable follow mode."
-  (interactive)
-  (split-into-two-columns)
-  (follow-mode 1))
-
-(defun split-into-three-columns ()
-  "Split the frame into three columns"
-  (interactive)
-  (delete-other-windows)
-  (split-window-horizontally)
-  (split-window-horizontally)
-  (balance-windows))
-
-(defun split-into-three-columns-and-cycle ()
-  "Split the frame into three columns and select new buffers in the other windows. The current window
-will be in the middle of the new layout."
-  (interactive)
-  (split-into-three-columns)
-  (next-buffer)
-  (select-next-window)
-  (select-next-window)
-  (next-buffer)
-  (next-buffer)
-  (select-next-window)
-  (select-next-window))
-
-(defun split-into-three-columns-and-follow ()
-  "Split the frame into three columns and turn on follow mode."
-  (interactive)
-  (split-into-three-columns)
-  (follow-mode 1))
-
-;;----------------------------------------------------------------------
 ;; Utility functions
 
 (require 'cl)
@@ -78,6 +18,9 @@ will be in the middle of the new layout."
   (apply #'mrc/directory-join
          (cons (file-name-directory (or load-file-name buffer-file-name))
                path-elements)))
+
+;; Functions for making nice balanced columns in a frame.
+(load-file (mrc/relative-directory-join "balanced-frame-splits.el"))
 
 ;;----------------------------------------------------------------------
 ;; Configuration and Minor Modes
